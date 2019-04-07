b815b5eb9a457e05777da550be38927ab10a1044
Graham Preston <preston.gr@husky.neu.edu>
2019-04-07 15:00:49 -0400

    Import off

--- server/fcserver.py
+++ server/fcserver.py
@@ -1,37 +1,44 @@
+import sys
 import subprocess
 import asyncio
 import atexit
 
 from types import FunctionType
 from definitions import ROOT_DIR
+from webcandy import app
 
 
 class FCServer:
     """
     Controller for Fadecandy server.
     """
-
-    def __init__(self):
-        self._server_running: bool = False
-        self._fcserver_proc: subprocess.Popen = None
+    _server_running: bool = False
+    _fcserver_proc: subprocess.Popen = None
 
     def start(self) -> None:
         """
         Run the Fadecandy server. Terminates on program exit.
         """
         async def _go(stop_fn: FunctionType):
-            _fcserver_proc = subprocess.Popen(ROOT_DIR + '/bin/fcserver.exe')
-            atexit.register(stop_fn)
+            if sys.platform == 'win32':
+                server = 'fcserver.exe'
+            elif sys.platform == 'darwin':
+                server = 'fcserver-osx'
+            else:
+                server = 'fcserver-rpi'
+            _fcserver_proc = subprocess.Popen(ROOT_DIR + '/bin/' + server)
+            atexit.register(stop_fn)  # TODO: Does this even do anything?
             return _fcserver_proc
 
         if not self._server_running:
-            self._server_running = True
             self._fcserver_proc = asyncio.run(_go(self.stop))
+            self._server_running = True
+            app.logger.info('Started fcserver')
 
     def stop(self) -> None:
         """
-        Stop the Fadecandy serer.
+        Stop the Fadecandy server.
         """
-        print('Stopping fcserver...')
-        self._server_running = False
         self._fcserver_proc.terminate()
+        self._server_running = False
+        app.logger.info('Stopped fcserver')
--- server/scripts/__init__.py
+++ server/scripts/__init__.py
@@ -3,3 +3,4 @@ from . import strobe
 from . import rainbow_scroll
 from . import rainbow_strobe
 from . import solid_color
+from . import off
