cd2c23d7c1c7f73bf1f84e437d8de5ca9bbaf057
Graham Preston <gpreston@imprivata.com>
2019-04-11 09:07:53 -0400

    Add test_opcutil.py

--- server/scripts/opcutil.py
+++ server/scripts/opcutil.py
@@ -1,9 +1,54 @@
+import re
 import time
 import math
 
+from typing import Tuple, List
 
-def even_spread(colors, num_leds):
-    """Evenly spreads out the colors across the LEDs in order."""
+
+def is_color(s: str) -> bool:
+    """
+    Determine if ``s`` is a color hex in the format #RRGGBB.
+
+    :param s: the string to check
+    :return: ``True`` if ``s`` fits the pattern; ``False`` otherwise
+    """
+    return bool(re.match(r'^#[A-Fa-f0-9]{6}$', s))
+
+
+def get_color(hex_str: str) -> Tuple[int, int, int]:
+    """
+    Calculate a 3-tuple representing the color in the given hex.
+
+    :param hex_str: the desired color in the format #RRGGBB.
+    :return: a 3-tuple of RGB values ``hex_str`` represents
+    :raises ValueError: if ``hex_str`` is improperly formatted
+    """
+    if not is_color(hex_str):
+        hex_repr = f"'{hex_str}'" if type(hex_str) is str else str(hex_str)
+        raise ValueError("Please provide a color in the format '#RRGGBB'. "
+                         f"Received {hex_repr}.")
+
+    red = int(hex_str[1:3], 16)
+    blue = int(hex_str[3:5], 16)
+    green = int(hex_str[5:7], 16)
+
+    return red, blue, green
+
+
+def even_spread(colors: List, num_leds: int) -> List:
+    """
+    Evenly spread out the colors across the LEDs in order.
+
+    :param colors: a list of values to spread
+    :param num_leds: the length of the list to create from the given colors
+    :return: a list consisting of ``colors`` spread across ``num_leds`` indices
+    :raises ValueError: if ``colors`` is empty or ``num_leds`` is negative
+    """
+    if len(colors) < 1:
+        raise ValueError('no colors provided')
+    if num_leds < 0:
+        raise ValueError('cannot spread across a negative number of LEDs')
+
     pixels = []
     pixels_per_color = math.floor(num_leds / len(colors))
     remainder = num_leds % len(colors)
@@ -15,11 +60,25 @@ def even_spread(colors, num_leds):
     return pixels
 
 
-def spread(colors, num_leds, pixels_per_color):
-    """Spreads out the colors across the LEDs in order using the
-    specified number of pixels per color."""
-    pixels = []
+def spread(colors: List, num_leds: int, pixels_per_color: int):
+    """
+    Spread out the colors across the LEDs in order using the specified number
+    of pixels per color.
 
+    :param colors: the values to spread
+    :param num_leds: the length of the list to create from the given colors
+    :param pixels_per_color: the length of each color sequence
+    :return: a list consisting of sequences of values in ``colors`` of length
+       ``pixels_per_color`` each
+    :raises ValueError: if ``colors`` is empty or either ``num_leds`` or
+        ``pixels_per_color`` is negative
+    """
+    if len(colors) < 1:
+        raise ValueError('no colors provided')
+    if num_leds < 0 or pixels_per_color < 0:
+        raise ValueError('cannot spread across a negative number of LEDs')
+
+    pixels = []
     color_index = 0
     leds_left = num_leds
     while leds_left > 0:
@@ -30,8 +89,15 @@ def spread(colors, num_leds, pixels_per_color):
     return pixels
 
 
-def shift(current, goal, p):
-    """Returns current shifted towards goal by a factor of p (proportion)."""
+def shift(current: Tuple[int, int, int], goal: Tuple[int, int, int], p: float):
+    """
+    Shift a color towards another.
+
+    :param current: a 3-tuple representing the starting color
+    :param goal: a 3-tuple representing the color to shift towards
+    :param p: a value indicating how far to shift (0 => no shift, 1 => ``goal``)
+    :return: the shifted color
+    """
     return [current[i] + ((goal[i] - current[i]) * p) for i in range(3)]
 
 
---
+++ server/tests/scripts/test_opcutil.py
@@ -0,0 +1,59 @@
+import pytest
+
+from scripts import opcutil
+
+valid_colors = ['#000000', '#123456', '#987abc', '#abcdef', '#ffffff']
+invalid_colors = ['#fffffg', 'fail', 'abcdef', '#abcdef0', '#abcde', '#abc']
+
+
+def test_is_color():
+    for color in valid_colors:
+        assert opcutil.is_color(color)
+
+    for color in invalid_colors:
+        assert not opcutil.is_color(color)
+
+
+def test_get_color():
+    get_color_match = "Please provide a color in the format '#RRGGBB'.*"
+    for color in invalid_colors:
+        with pytest.raises(ValueError, match=get_color_match):
+            opcutil.get_color(color)
+
+    assert opcutil.get_color('#000000') == (0, 0, 0)
+    assert opcutil.get_color('#0eff32') == (14, 255, 50)
+
+
+def test_even_spread():
+    with pytest.raises(ValueError, match='no colors provided'):
+        opcutil.even_spread([], 5)
+    with pytest.raises(ValueError,
+                       match='cannot spread across a negative number of LEDs'):
+        opcutil.even_spread(['c1', 'c2'], -3)
+
+    assert opcutil.even_spread(['c1', 'c2', 'c3'], 9) == ['c1', 'c1', 'c1',
+                                                          'c2', 'c2', 'c2',
+                                                          'c3', 'c3', 'c3']
+    assert opcutil.even_spread(['c1', 'c2', 'c3'], 11) == ['c1', 'c1', 'c1',
+                                                           'c2', 'c2', 'c2',
+                                                           'c3', 'c3', 'c3',
+                                                           'c1', 'c1']
+    assert opcutil.even_spread(['c1'], 0) == []
+    assert opcutil.even_spread(['c1'], 5) == ['c1', 'c1', 'c1', 'c1', 'c1']
+
+
+def test_spread():
+    with pytest.raises(ValueError, match='no colors provided'):
+        opcutil.spread([], 6, 2)
+    with pytest.raises(ValueError,
+                       match='cannot spread across a negative number of LEDs'):
+        opcutil.spread(['c1', 'c2'], -3, 1)
+    with pytest.raises(ValueError,
+                       match='cannot spread across a negative number of LEDs'):
+        opcutil.spread(['c1', 'c2'], 6, -2)
+
+    assert opcutil.spread(['c1'], 5, 2) == ['c1', 'c1', 'c1', 'c1', 'c1']
+    assert opcutil.spread(['c1', 'c2'], 8, 2) == ['c1', 'c1', 'c2', 'c2', 'c1',
+                                                  'c1', 'c2', 'c2']
+    assert opcutil.spread(['c1', 'c2', 'c3'], 7, 3) == ['c1', 'c1', 'c1', 'c2',
+                                                        'c2', 'c2', 'c3']
