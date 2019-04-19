80c561e88f49e07ed41bc5ef59456f5ea7bbced3
Graham Preston <graham.preston@gmail.com>
2019-04-19 10:30:11 -0400

    Add documentation

--- server/webcandy/models.py
+++ server/webcandy/models.py
@@ -4,6 +4,7 @@ from itsdangerous import (
     BadSignature,
     SignatureExpired
 )
+from typing import Optional
 from config import Config
 from .extensions import db
 
@@ -16,18 +17,29 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    def set_password(self, password):
+    def set_password(self, password: str) -> None:
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
 
-    def generate_auth_token(self, expiration=600):
+    def generate_auth_token(self, expiration: int = 3600) -> str:
+        """
+        Generate an authentication token storing this ``User``'s ID.
+        :param expiration: the number of seconds the token should expire in
+        :return: the generated authentication token
+        """
         s = Serializer(Config.SECRET_KEY, expires_in=expiration)
         return s.dumps({'id': self.id})
 
     @staticmethod
-    def verify_auth_token(token):
+    def verify_auth_token(token: str) -> Optional['User']:
+        """
+        Verify an authentication token.
+        :param token: the token to verify
+        :return: the ``User`` associated with the token; ``None`` if token is
+            invalid or expired
+        """
         s = Serializer(Config.SECRET_KEY)
         try:
             data = s.loads(token)
