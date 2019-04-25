fb6e2a6f023aacde41aa86dcfb8021f86552c60d
Graham Preston <graham.preston@gmail.com>
2019-04-25 09:31:38 -0400

    Create WebcandyClientManager

---
+++ server/src/webcandy/client_manager.py
@@ -0,0 +1,29 @@
+import socket
+import multiprocessing
+
+
+def _accept_connections(sock: socket.socket):
+    while True:
+        conn, addr = sock.accept()
+        with conn:
+            print(f'Connected {addr}')
+            conn.sendall(b'Hello client!')
+
+
+class WebcandyClientManager:
+    """
+    Class to manage client socket connections.
+    """
+
+    def __init__(self, host: str = '127.0.0.1', port: int = 6543):
+        # test if manager is already running
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as test_sock:
+            result = test_sock.connect_ex((host, port))
+
+        if result == 10061:  # nothing running
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.bind((host, port))
+                sock.listen()
+                p = multiprocessing.Process(target=_accept_connections,
+                                            args=(sock,))
+                p.start()
