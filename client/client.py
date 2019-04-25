d67fb1cc66aaaf99297163499cde2ebcf5ba3329
Graham Preston <graham.preston@gmail.com>
2019-04-25 09:31:38 -0400

    Create WebcandyClientManager

--- client/client.py
+++ client/client.py
@@ -6,7 +6,7 @@ class WebcandyClient:
     Webcandy client to receive light configuration submission data from server.
     """
 
-    def __init__(self, host: str = '127.0.0.1', port: int = 65432):
+    def __init__(self, host: str = '127.0.0.1', port: int = 6543):
         self.host = host
         self.port = port
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- server/src/run.py
+++ server/src/run.py
@@ -9,11 +9,11 @@ debug = True
 if __name__ == '__main__':
     app = create_app()
 
-    # Start fcserver if not already running
+    # start fcserver if not already running
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         result = sock.connect_ex(('127.0.0.1', 7890))
 
-    if result == 10061:  # Nothing running
+    if result == 10061:  # nothing running
         fcserver.start()
 
     app.run(debug=debug)
