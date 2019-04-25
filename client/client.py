61518f4d2acb6c1e589b4b02dfa52033a2d5646d
Graham Preston <preston.gr@husky.neu.edu>
2019-04-24 23:32:19 -0400

    Basic client-server sandbox

---
+++ client/client.py
@@ -0,0 +1,23 @@
+import socket
+
+
+class WebcandyClient:
+    """
+    Webcandy client to receive light configuration submission data from server.
+    """
+
+    def __init__(self, host: str = '127.0.0.1', port: int = 65432):
+        self.host = host
+        self.port = port
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((host, port))
+
+        while True:
+            data = self.s.recv(1024)
+            if not data:
+                break
+            print(data)
+
+
+if __name__ == '__main__':
+    client = WebcandyClient()
---
+++ client/server.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f'Connected {".".join(addr)}')
+            conn.sendall(b'Hello client!')
