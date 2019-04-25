439e9ecdb8fb6d5134222fceedab15db4b71583b
Graham Preston <graham.preston@gmail.com>
2019-04-25 15:08:29 -0400

    Basic send functionality

--- server/src/webcandy/app.py
+++ server/src/webcandy/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from config import Config
 from definitions import ROOT_DIR
 from . import routes
-from .extensions import db, migrate, fcserver, controller
+from .extensions import db, migrate, fcserver, controller, manager
 
 
 def create_app():
@@ -14,10 +14,9 @@ def create_app():
     app = Flask(__name__, static_folder=f'{ROOT_DIR}/static/dist',
                 template_folder=f'{ROOT_DIR}/static')
     app.config.from_object(Config)
+    app.logger.setLevel(logging.DEBUG)
     register_extensions(app)
     register_views(app)
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s %(levelname)s: %(message)s')
     return app
 
 
@@ -29,6 +28,7 @@ def register_extensions(app):
     migrate.init_app(app, db)
     fcserver.init_app(app)
     controller.init_app(app)
+    manager.init_app(app)
 
 
 def register_views(app):
