30544b765fd35c774a0654b0f2d635a7112bd52d
Graham Preston <graham.preston@gmail.com>
2019-04-26 12:59:42 -0400

    Add TODO

--- server/src/run.py
+++ server/src/run.py
@@ -3,6 +3,11 @@ import socket
 from webcandy.app import create_app
 from webcandy.extensions import fcserver, manager
 
+# TODO: Make manager work in debug mode
+# - What happens right now is everything is initialized, but the server restarts
+#   causing a new manager to be created
+# - Connections are initialized in the new manager, but it seems that the API
+#   send route uses the old manager, which has no initialized connections
 debug = False
 
 
