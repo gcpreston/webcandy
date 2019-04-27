553bcff7f3f50418df0d840321315a72ccda41de
Graham Preston <preston.gr@husky.neu.edu>
2019-04-26 21:23:08 -0400

    stripe -> stripes

--- client/opclib/patterns/__init__.py
+++ client/opclib/patterns/__init__.py
@@ -2,5 +2,5 @@ from .fade import Fade
 from .strobe import Strobe
 from .scroll import Scroll
 from .solid_color import SolidColor
-from .stripe import Stripe
+from .stripes import Stripes
 from .off import Off
