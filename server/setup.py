4b1fe8c55dd03b065d6defd804b972b4dc9de4a0
Graham Preston <preston.gr@husky.neu.edu>
2019-06-22 19:48:17 -0400

    Update setup.py

--- server/setup.py
+++ server/setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
+with open('README.md') as readme:
+    long_description = readme.read()
+
 setup(
     name='webcandy',
+    version='0.0.1',
+    author='Graham Preston',
+    author_email='graham.preston@gmail.com',
+    description='API and web interface for Fadecandy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gcpreston/webcandy',
     packages=['webcandy'],
     zip_safe=False,
     install_requires=[
